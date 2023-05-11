67233b50105194716fc982e9d80c00b55fafbc67
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-11 15:37:58 +0200

    defined and testing first functionalities

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-console.log ("prova");
+console.log ("JS Loaded!");
+
+let userName;
+let userSurname;
+let userColor;
+
+const pagePrompt = prompt(userName);
+document.getElementById("test").innerHTML = pagePrompt;
+
+const pageTwoPrompt = prompt(userSurname);
+document.getElementById("test-two").innerHTML = pageTwoPrompt;
