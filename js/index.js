63d5f965a2ece419b60b8f86b6019b566eac6c13
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-11 16:22:12 +0200

    chnaged id element and added new variable

--- js/index.js
+++ js/index.js
@@ -4,14 +4,17 @@ let userName = prompt('Scrivi il tuo nome:');
 let userSurname = prompt('Scrivi il tuo cognome:');
 let userColor = prompt('Scrivi il tuo colore preferito:');
 
-// Console Output Just to make sure 
-// everything is working correctly
-console.log("Result of prompts:" + userName+userSurname+userColor);
+let userNumber = parseInt(
+    prompt('Scrivi il tuo numero fortunato (Al numero inserito ci sarà un aggiunta di 10)'));
 
 // Result Variable of the prompts
-const userInputResults = userName + userSurname + userColor;
+const userInputResults = userName + userSurname + userColor + userNumber;
 
-const pagePrompt = document.getElementById("test").innerHTML = userInputResults;
+// Console Output Just to make sure 
+// everything is working correctly
+console.log("Result of prompts:" + userInputResults);
+
+const pagePrompt = document.getElementById("result-prompts").innerHTML = userInputResults;
 
 // const pageTwoPrompt = prompt(userSurname);
 // document.getElementById("test-two").innerHTML = pageTwoPrompt;
